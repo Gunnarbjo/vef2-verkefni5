41908e19287b848572502a6e06b6b8fb85df5ec6
Gunnarbjo <gbt6@hi.is>
2022-03-31 15:47:41 +0000

    fyrir heroku

--- vef2-verkefni5/pages/Nýskráning.js
+++ vef2-verkefni5/pages/Nýskráning.js
@@ -1,9 +1,1 @@
 
-export default function Home() {
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>Viðburðir á næstunni</title>
-          <meta name="Front page" content="Front page" />
-        </Head>
-  

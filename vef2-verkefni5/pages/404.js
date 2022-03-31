caa988715dc49108a1ef69adac6c4381f7f97f67
Gunnarbjo <gbt6@hi.is>
2022-03-31 18:23:57 +0000

    bæta við sql

--- vef2-verkefni5/pages/404.js
+++ vef2-verkefni5/pages/404.js
@@ -1,4 +1,11 @@
+import styles from '../styles/Home.module.css'
+
 export default function Custom404() {
-  return <h1>Can't find the site man....<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> click to go back</a></h1>
-  
+  return (<body class={styles.fourofour}>
+    <div>
+    <h1>Can't find the site man....</h1>
+    <p><a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> click to go back</a></p>
+    <p><a href=".."> click to get Rick rolled</a></p>
+  </div>
+  </body>)
 }
